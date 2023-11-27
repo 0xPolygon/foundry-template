f929f8b83d27dc003c0763dffd9efbb1bed485bd
Zero Ekkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2023-11-27 19:57:53 +0100

    feat: robust extractor

--- script/util/extractor.js
+++ script/util/extractor.js
@@ -1,50 +1,40 @@
 const fs = require("fs");
 const path = require("path");
-const { exec } = require("child_process");
-const Util = require("util");
-const asyncExec = Util.promisify(exec);
+const { execSync } = require("child_process");
 
 // ========== ABOUT ==========
 
 /*
 
 Given the latest broadcast file,
-Update the deployment history and latest data for the chain.
+Updates the deployment history and latest data for the chain.
 
-Notes:
-
-- The script assumes that the proxy is created immediately after the implementation contract.
-- Only TransparentUpgradeableProxy by OpenZeppelin is supported.
+Note: Only TransparentUpgradeableProxy by OpenZeppelin is supported at the moment.
 
 */
 
-// ========== INPUTS ==========
-
-const scriptName = process.argv[2]; // Replace with the appropriate index for scriptName
-const chainId = process.argv[3]; // Replace with the appropriate index for chainId
-
-async function main() {
+async function main(scriptName, chainId) {
   console.log("Extracting...");
 
-  // ========== APPEND TO HISTORY ==========
+  // ========== PREPARE FILES ==========
 
-  // Used for getVersion() helper
+  // For getVersion helper
   const config = JSON.parse(fs.readFileSync(path.join(__dirname, "../config.json"), "utf-8"));
   const rpcUrl = config.defaultRpc[chainId] || process.env.RPC_URL || "http://127.0.0.1:8545";
 
+  // Latest broadcast
   const filePath = path.join(__dirname, `../../broadcast/${scriptName}/${chainId}/run-latest.json`);
-  // Load JSON data from file
   const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
+  // Previously extracted data
   const recordFilePath = path.join(__dirname, `../../deployments/json/${chainId}.json`);
-
   let recordData;
 
+  // Try to read previously extracted data
   try {
-    // Try to read the existing record.json file
     recordData = JSON.parse(fs.readFileSync(recordFilePath, "utf8"));
   } catch (error) {
-    // If the file doesn't exist, create a new object
+    // If the file doesn't exist, create a new JSON
     recordData = {
       chainId: chainId,
       latest: {},
@@ -52,149 +42,190 @@ async function main() {
     };
   }
 
-  // If history is not empty, check if the latest entry is the same as the current commit
+  // Abort if commit processed
   if (recordData.history.length > 0) {
     const latestEntry = recordData.history[recordData.history.length - 1];
     if (latestEntry.commitHash === jsonData.commit) {
-      // If the latest entry is the same as the current commit, exit the script
-      console.log(`Commit ${jsonData.commit} already processed. Aborted.`);
-      return;
+      console.error(`Commit ${jsonData.commit} already processed. Aborted.`);
+      process.exit(1);
     }
   }
 
-  await prepareArtifacts();
-
-  // Filter transactions with "transactionType" as "CREATE"
-  const createTransactions = jsonData.transactions.filter((transaction) => transaction.transactionType === "CREATE");
-
-  // Initialize an object to store the items
-  const deploymentItems = {};
-
-  // Iterate through the filtered transactions
-  for (let i = 0; i < createTransactions.length; i++) {
-    const currentTransaction = createTransactions[i];
-
-    // Inside the loop where you process transactions
-    if (i < createTransactions.length - 1 && createTransactions[i + 1].contractName === "TransparentUpgradeableProxy") {
-      const upgradeableItem = {
-        implementation: currentTransaction.contractAddress,
-        proxyAdmin: createTransactions[i + 1].additionalContracts[0].address,
-        address: createTransactions[i + 1].contractAddress,
-        proxy: true,
-        version: (await getVersion(createTransactions[i + 1].contractAddress, rpcUrl)).version,
-        proxyType: "TransparentUpgradeableProxy",
-        deploymentTxn: createTransactions[i + 1].hash,
-        input: extractConstructorAndInitializeInputs(getABI(currentTransaction.contractName), {
-          constructor: currentTransaction.arguments,
-          // dump initData
-          initialize: createTransactions[i + 1].arguments[2],
-        }),
-      };
-
-      deploymentItems[currentTransaction.contractName] = upgradeableItem;
-
-      // Skip the next iteration to avoid processing the proxy again
-      i++;
-    } else {
-      const maybeNonUpgradeableItem = {
-        address: currentTransaction.contractAddress,
-        proxy: false,
-        version: (await getVersion(currentTransaction.contractAddress, rpcUrl)).version,
-        deploymentTxn: currentTransaction.hash,
-        input: extractConstructorAndInitializeInputs(getABI(currentTransaction.contractName), {
-          constructor: currentTransaction.arguments,
-        }),
-      };
-
-      // Check if a contract with the same name exists in the latest data
-      if (recordData.latest.hasOwnProperty(currentTransaction.contractName)) {
-        const matchedItem = recordData.latest[currentTransaction.contractName];
-
-        // Check if the latest item has a proxy
-        if (matchedItem.proxy) {
-          // Update the current object with data from the latest item
-          maybeNonUpgradeableItem.implementation = currentTransaction.contractAddress;
-          maybeNonUpgradeableItem.proxyAdmin = matchedItem.proxyAdmin;
-          maybeNonUpgradeableItem.address = matchedItem.address;
-          maybeNonUpgradeableItem.proxy = true;
-          maybeNonUpgradeableItem.proxyType = matchedItem.proxyType;
-          maybeNonUpgradeableItem.deploymentTxn = matchedItem.deploymentTxn;
-        }
-
-        // Define the order of fields
-        const fieldOrder = [
-          "implementation",
-          "proxyAdmin",
-          "address",
-          "proxy",
-          "version",
-          "proxyType",
-          "deploymentTxn",
-          "input",
-        ];
-
-        // Create the object with the desired field order
-        const upgradedItem = {};
-        for (const fieldName of fieldOrder) {
-          upgradedItem[fieldName] = maybeNonUpgradeableItem[fieldName];
-        }
-
-        deploymentItems[currentTransaction.contractName] = upgradedItem;
-      } else {
-        deploymentItems[currentTransaction.contractName] = maybeNonUpgradeableItem;
-      }
-    }
-  }
-
-  // Now you have named items in the `deploymentItems` object
-
-  // Create a new JSON with the desired format
-  const historyItem = { contracts: deploymentItems, timestamp: jsonData.timestamp, commitHash: jsonData.commit };
-
-  // Append the historyItem to the history array
-  recordData.history.push(historyItem);
+  // Generate Forge artifacts
+  prepareArtifacts();
 
   // ========== UPDATE LATEST ==========
 
-  for (const contractName in historyItem.contracts) {
-    if (historyItem.contracts.hasOwnProperty(contractName)) {
-      const contractData = historyItem.contracts[contractName];
+  const upgradeableTemplate = {
+    implementation: "",
+    address: "",
+    proxy: true,
+    version: "",
+    proxyType: "TransparentUpgradeableProxy",
+    deploymentTxn: "",
+    proxyAdmin: "",
+    input: {},
+  };
 
-      // Check if an item with the same contract name exists in the latest object
-      if (recordData.latest[contractName]) {
-        // Update the existing entry in the latest object
-        recordData.latest[contractName] = {
-          ...recordData.latest[contractName], // Preserve existing data
-          ...contractData, // Update with new data from historyItem
-          timestamp: historyItem.timestamp, // Add timestamp
-          commitHash: historyItem.commitHash, // Add commitHash
-        };
-        // Exclude the 'input' field from the latest object
-        delete recordData.latest[contractName].input;
+  const nonUpgradeableTemplate = {
+    address: "",
+    proxy: false,
+    version: "",
+    deploymentTxn: "",
+    input: {},
+  };
+
+  // Filter CREATE transactions
+  const createTransactions = jsonData.transactions.filter((transaction) => transaction.transactionType === "CREATE");
+
+  // For history
+  const contracts = {};
+
+  // Iterate over transactions
+  for (let i = 0; i < createTransactions.length; i++) {
+    const currentTransaction = createTransactions[i];
+    const contractName = currentTransaction.contractName;
+
+    // ====== TYPE: CONTRACT NOT PROXY =====
+    if (contractName !== "TransparentUpgradeableProxy") {
+      // Contract exists in latest
+      if (recordData.latest.hasOwnProperty(contractName)) {
+        const matchedItem = recordData.latest[contractName];
+
+        // The latest is upgradeable
+        if (matchedItem.proxy) {
+          // CASE: New implementation created
+          const upgradeableItem = {
+            ...upgradeableTemplate,
+            implementation: currentTransaction.contractAddress,
+            proxyAdmin: matchedItem.proxyAdmin,
+            address: matchedItem.address,
+            proxy: true,
+            version: (await getVersion(matchedItem.address, rpcUrl)).version,
+            proxyType: matchedItem.proxyType,
+            deploymentTxn: matchedItem.deploymentTxn,
+            input: {
+              constructor: matchConstructorInputs(getABI(contractName), currentTransaction.arguments),
+              initializationTxn: "TODO",
+            },
+          };
+
+          // Append it to history item
+          contracts[contractName] = upgradeableItem;
+          // Update latest item
+          let copyOfUpgradeableItem = { ...upgradeableItem };
+          delete copyOfUpgradeableItem.input;
+          copyOfUpgradeableItem.timestamp = jsonData.timestamp;
+          copyOfUpgradeableItem.commitHash = jsonData.commit;
+          recordData.latest[contractName] = copyOfUpgradeableItem;
+        } else {
+          // The latest wasn't upgradeable
+          // CASE: Duplicate non-upgradeable contract
+          // TODO Allow if newer version.
+          console.error(`${contractName} is duplicate non-upgradeable. Aborted.`);
+          process.exit(1);
+        }
       } else {
-        // Add a new entry to the latest object
-        recordData.latest[contractName] = {
-          ...contractData, // Add new data from historyItem
-          timestamp: historyItem.timestamp, // Add timestamp
-          commitHash: historyItem.commitHash, // Add commitHash
-        };
-        // Exclude the 'input' field from the latest object
-        delete recordData.latest[contractName].input;
+        // Contract didn't exist in latest
+
+        // Search for proxy in subsequent transactions
+        let proxyFound = false;
+
+        for (let j = i + 1; j < createTransactions.length; j++) {
+          const nextTransaction = createTransactions[j];
+          // Proxy found
+          if (
+            nextTransaction.contractName === "TransparentUpgradeableProxy" &&
+            nextTransaction.arguments[0] === currentTransaction.contractAddress
+          ) {
+            // CASE: New upgradeable contract
+            const upgradeableItem = {
+              ...upgradeableTemplate,
+              implementation: currentTransaction.contractAddress,
+              proxyAdmin: nextTransaction.additionalContracts[0].address,
+              address: nextTransaction.contractAddress,
+              proxy: true,
+              version: (await getVersion(nextTransaction.contractAddress, rpcUrl)).version,
+              proxyType: nextTransaction.contractName,
+              deploymentTxn: nextTransaction.hash,
+              input: {
+                constructor: matchConstructorInputs(getABI(contractName), currentTransaction.arguments),
+                initializationTxn: "TODO",
+              },
+            };
+
+            // Append it to history item
+            contracts[contractName] = upgradeableItem;
+            // Update latest item
+            let copyOfUpgradeableItem = { ...upgradeableItem };
+            delete copyOfUpgradeableItem.input;
+            copyOfUpgradeableItem.timestamp = jsonData.timestamp;
+            copyOfUpgradeableItem.commitHash = jsonData.commit;
+            recordData.latest[contractName] = copyOfUpgradeableItem;
+
+            proxyFound = true;
+          }
+        }
+        // Didn't find proxy
+        if (!proxyFound) {
+          // CASE: New non-upgradeable contract
+          const nonUpgradeableItem = {
+            ...nonUpgradeableTemplate,
+            address: currentTransaction.contractAddress,
+            version: (await getVersion(currentTransaction.contractAddress, rpcUrl)).version,
+            deploymentTxn: currentTransaction.hash,
+            input: { constructor: matchConstructorInputs(getABI(contractName), currentTransaction.arguments) },
+          };
+
+          // Append it to history item
+          contracts[contractName] = nonUpgradeableItem;
+          // Update latest item
+          let copyOfNonUpgradeableItem = { ...nonUpgradeableItem };
+          delete copyOfNonUpgradeableItem.input;
+          copyOfNonUpgradeableItem.timestamp = jsonData.timestamp;
+          copyOfNonUpgradeableItem.commitHash = jsonData.commit;
+          recordData.latest[contractName] = copyOfNonUpgradeableItem;
+        }
+      }
+    } else {
+      // ===== TYPE: PROXY =====
+
+      // Check if proxy has been processed
+      for (const contractName in recordData.latest) {
+        if (recordData.latest.hasOwnProperty(contractName)) {
+          const latestItem = recordData.latest[contractName];
+          if (latestItem.address === currentTransaction.contractAddress) {
+            // CASE: Proxy done
+            break;
+          } else {
+            // CASE: Unexpected proxy
+            console.error(`Unexpected proxy encountered. Aborted.`);
+            process.exit(1);
+          }
+        }
       }
     }
   }
 
-  // ========== SAVE CHANGES ==========
+  // ========== APPEND TO HISTORY ==========
 
-  // Ensure the directory exists before writing the file
+  recordData.history.push({ contracts, timestamp: jsonData.timestamp, commitHash: jsonData.commit });
+
+  /* ========== SAVE CHANGES ==========
+
+  // Create file if it doesn't exist
   const directoryPath = path.dirname(recordFilePath);
   if (!fs.existsSync(directoryPath)) {
-    fs.mkdirSync(directoryPath, { recursive: true }); // Create the directory if it doesn't exist
+    fs.mkdirSync(directoryPath, { recursive: true });
   }
 
-  // Write the updated object back to the record.json file
+  // Write to file
   fs.writeFileSync(recordFilePath, JSON.stringify(recordData, null, 2), "utf8");
-  console.log(`Extraction complete.`);
+  */
+
+  console.log(`Extraction complete!`);
+
+  return recordData;
 }
 
 main();
@@ -235,37 +266,17 @@ async function getVersion(contractAddress, rpcUrl) {
   }
 }
 
-// IN: contract ABI and input (constructor and initialize) values
+// IN: contract ABI and input values
 // OUT: mappings of input names to values
-// Note: If .initialize is a string, .initData will be dumped.
-function extractConstructorAndInitializeInputs(abi, inputData) {
+function matchConstructorInputs(abi, inputData) {
   const inputMapping = {};
 
-  // Find the constructor in the ABI by type "constructor"
   const constructorFunc = abi.find((func) => func.type === "constructor");
 
-  // Find the initialize function in the ABI by name "initialize"
-  const initializeFunc = abi.find((func) => func.type === "function" && func.name === "initialize");
-
-  if (constructorFunc && inputData.constructor) {
-    // Match constructor inputs with their names
-    inputMapping.constructor = {};
+  if (constructorFunc && inputData) {
     constructorFunc.inputs.forEach((input, index) => {
-      inputMapping.constructor[input.name] = inputData.constructor[index];
+      inputMapping[input.name] = inputData[index];
     });
-  }
-
-  if (initializeFunc) {
-    if (typeof inputData.initialize === "string") {
-      // If initialize is a string, create an "initData" field
-      inputMapping.initData = inputData.initialize;
-    } else if (inputData.initialize) {
-      // Match initialize inputs with their names
-      inputMapping.initialize = {};
-      initializeFunc.inputs.forEach((input, index) => {
-        inputMapping.initialize[input.name] = inputData.initialize[index];
-      });
-    }
   }
 
   return inputMapping;
@@ -280,12 +291,12 @@ function getABI(contractName) {
   return abi;
 }
 
-// Note: Makes sure contract artifacts are up-to-date.
-async function prepareArtifacts() {
+// Note: Ensures contract artifacts are up-to-date.
+function prepareArtifacts() {
   console.log(`Preparing artifacts...`);
 
-  await asyncExec("forge clean");
-  await asyncExec("forge build");
+  execSync("forge clean");
+  execSync("forge build");
 
-  console.log(`Artifacts ready. Continuing...`);
+  console.log(`Artifacts ready. Continuing.`);
 }
