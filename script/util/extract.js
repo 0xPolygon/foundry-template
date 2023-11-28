d7e9dd680b864a575590bf6197ae9e5049505492
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2023-11-28 16:52:25 +0530

    feat: skipJsonFlag

--- script/util/extract.js
+++ script/util/extract.js
@@ -1,3 +1,5 @@
+const { readFileSync, existsSync } = require("fs");
+const path = require("path");
 const { extractAndSaveJson } = require("./extractor.js");
 const { generateAndSaveMarkdown } = require("./generateMarkdown.js");
 /**
@@ -7,23 +9,35 @@ const { generateAndSaveMarkdown } = require("./generateMarkdown.js");
  *  currently only supports TransparentUpgradeableProxy pattern
  */
 async function main() {
-  validateInputs();
-  let [chainId, scriptName] = process.argv.slice(2);
-  if (!scriptName?.length) scriptName = "Deploy.s.sol";
-  generateAndSaveMarkdown(await extractAndSaveJson(scriptName, chainId));
+  let [chainId, scriptName, skipJsonFlag] = validateAndExtractInputs();
+  let json;
+  if (!skipJsonFlag.length) json = await extractAndSaveJson(scriptName, chainId);
+  else {
+    console.log("Skipping json extraction, using existing json file");
+    const recordFilePath = path.join(__dirname, `../../deployments/json/${chainId}.json`);
+    if (!existsSync(recordFilePath)) throw new Error(`error: ${recordFilePath} does not exist`);
+    json = JSON.parse(readFileSync(recordFilePath, "utf-8"));
+  }
+  generateAndSaveMarkdown(json);
 }
 
-function validateInputs() {
-  let [chainId, scriptName] = process.argv.slice(2);
+function validateAndExtractInputs() {
+  let [chainId, scriptName, skipJsonFlag] = process.argv.slice(2);
   let printUsageAndExit = false;
   if (!(typeof chainId === "string" && ["string", "undefined"].includes(typeof scriptName)) || chainId === "help") {
     if (chainId !== "help") console.log(`error: invalid inputs: ${JSON.stringify({ chainId, scriptName }, null, 0)}\n`);
     printUsageAndExit = true;
   }
+  if (typeof skipJsonFlag !== "undefined" && !["--skip-json", "-s"].includes(skipJsonFlag)) {
+    console.log(`error: invalid flag: ${JSON.stringify({ skipJsonFlag }, null, 0)}\n`);
+    printUsageAndExit = true;
+  }
   if (printUsageAndExit) {
-    console.log(`usage: node script/utils/extract.js {chainId} [scriptName = "Deploy.s.sol"]`);
+    console.log(`usage: node script/utils/extract.js {chainId} [scriptName = "Deploy.s.sol"] [--skip-json | -s]`);
     process.exit(1);
   }
+  if (!scriptName?.length) scriptName = "Deploy.s.sol";
+  return [chainId, scriptName, skipJsonFlag];
 }
 
 main();
