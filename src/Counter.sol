61bc3a731baf3b78a88af277c90b57c4155766fa
gretzke <daniel@gretzke.de>
2023-12-03 03:26:51 +0100

    bump solidity version

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.22;
+pragma solidity 0.8.23;
 
 import {ICounter, IVersioned} from "./interface/ICounter.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
